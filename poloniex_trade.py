53a0f35027ad8ef97e2567a0f83e58f4686d599b
= <383952780@qq.com>
2018-10-12 17:05:12 +0800

    增加任务池

--- poloniex_trade.py
+++ poloniex_trade.py
@@ -3,6 +3,7 @@ from component.cleaners import PoloniexTradeCleaner
 from component.limiters import StandardLimiter
 from component.ip_controllers import StandardController
 from component.exception_handler import PoloniexTradeExceptionHandler
+import time
 
 if __name__ == "__main__":
     limiter = StandardLimiter("Poloniex")
@@ -16,7 +17,9 @@ if __name__ == "__main__":
     )
 
     coinpairs = spider.get_coinpairs()
-    depth_url = "https://poloniex.com/public?command=returnTradeHistory&currencyPair="
+    end = int(time.time())
+    start = end - 3600 * 24
+    depth_url = "https://poloniex.com/public?command=returnTradeHistory&start=%s&end=%s&currencyPair=" % (start, end)
     url_list = [[depth_url + "_".join(coinpair.split("/")[::-1]), coinpair] for coinpair in coinpairs]
     spider.add_task(url_list)
 
